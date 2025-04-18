631f918f97dfeef69d580a6e2ec071909fa211d7
Matías Figueroa Contreras <matias.figueroa.c@usach.cl>
2025-04-18 16:15:16 -0400

    refactor(api): update line lenght formatter script

--- api/src/scripts/format.py
+++ api/src/scripts/format.py
@@ -3,12 +3,15 @@ import subprocess
 
 def run_command(command):
     result = subprocess.run(command, shell=True, check=True, text=True)
-    print(result.stdout)
+    print(result)
 
 
 def format_code():
     print("Running code formatters...")
     print("Running black...")
     run_command("poetry run black .")
+    print("Finished running black.")
+
     print("\nRunning isort...")
     run_command("poetry run isort .")
+    print("Finished running isort.")
