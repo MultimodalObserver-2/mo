cc0b1b00a6f1a4910e23cbd6fcfda891a198556e
Matías Figueroa Contreras <matias.figueroa.c@usach.cl>
2025-04-18 16:47:59 -0400

    feat(ui): add home page layout

---
+++ ui/src/renderer/src/core/pages/home/Home.tsx
@@ -0,0 +1,10 @@
+import styles from "./home.module.css"
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <section className={styles.playground}></section>
+      <section className={styles.panel}></section>
+    </main>
+  )
+}
--- ui/src/renderer/src/main.tsx
+++ ui/src/renderer/src/main.tsx
@@ -4,13 +4,14 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { BrowserRouter, Route, Routes } from "react-router"
 import MainLayout from "./core/layouts/MainLayout"
+import Home from "./core/pages/home/Home"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route element={<MainLayout />}>
-          <Route path="/" element={<div>Home</div>} />
+          <Route index path="/" element={<Home />} />
           <Route path="/plugins" element={<div>Plugins</div>} />
           <Route path="/settings" element={<div>Settings</div>} />
         </Route>
