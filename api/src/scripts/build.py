e0d411095e50f29ce21af04eb9764199d7bf3976
Matías Figueroa Contreras <matias.figueroa.c@usach.cl>
2025-04-29 12:08:05 -0400

    style(api): code format

--- api/src/scripts/build.py
+++ api/src/scripts/build.py
@@ -3,8 +3,7 @@ import subprocess
 
 def build():
     print("Generating executable...")
-    pyinstaller_cmd = ["pyinstaller",
-                       "mo_api.build.spec"]
+    pyinstaller_cmd = ["pyinstaller", "mo_api.build.spec"]
     subprocess.run(pyinstaller_cmd, check=True)
     print("Executable generated in dist folder.")
 
