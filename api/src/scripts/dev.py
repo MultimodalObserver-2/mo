899c74377f02ec06cfbf183be3c3c6bc7357ec97
Matías Figueroa Contreras <matias.figueroa.c@usach.cl>
2025-05-12 15:33:22 -0400

    fix(api): reload dirs dev script

--- api/src/scripts/dev.py
+++ api/src/scripts/dev.py
@@ -5,7 +5,7 @@ import uvicorn
 
 def start():
     os.environ["APP_ENV"] = "development"
-    uvicorn.run("api.main:app", host="localhost", port=8000, reload=True)
+    uvicorn.run("api.main:app", host="localhost", port=8000, reload=True, reload_dirs=["api/src/api", "src/api"])
 
 
 if __name__ == "__main__":
