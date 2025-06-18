f260ecae38839e343f23e455b79b59d275a1c3f9
Matías Figueroa Contreras <matias.figueroa.c@usach.cl>
2025-06-18 12:49:59 -0400

    feat(ui): expose shared module via import map for plugin access

---
+++ ui/src/renderer/shared/index.ts
@@ -0,0 +1,8 @@
+export { PluginBase } from "@renderer/core/plugin/types/PluginBase"
+export type { PluginMetadata } from "@renderer/core/plugin/types/PluginMetadata"
+export { Properties, Property } from "@renderer/core/plugin/types/Properties"
+export type {
+  PropertyType,
+  PropertySelectOption,
+  ModifiedCallback
+} from "@renderer/core/plugin/types/Properties"
